4c989d642c6855f9f1f4f37cae43696855e138d4
Harrison Latimer <harrisonlatimer@10-197-36-7.sds.oregonstate.edu>
2019-03-06 22:02:34 -0800

    edits

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ app.set('port',62619);
 var mysql = require('mysql');
 var pool = mysql.createPool({
   host  : 'classmysql.engr.oregonstate.edu',
-  user  : 'cs290_latimerh',
-  password: 'Chickens-123',
-  database: 'cs290_latimerh'
+  user  : 'cs340_latimerh',
+  password: '6395',
+  database: 'cs340_latimerh'
 });
 
 var con = mysql.createConnection({
   host: 'classmysql.engr.oregonstate.edu',
-  user: 'cs290_latimerh',
-  password: 'Chickens-123'
+  user: 'cs340_latimerh',
+  password: '6395'
 });
 
 con.connect(function(err) {
@@ -33,29 +33,30 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
-app.get('/reset-table',function(req,res,next){
+app.post('/insert_person',function(req,res,next){
   var context = {};
-  pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
-    var createString = "CREATE TABLE workouts("+
-    "id INT PRIMARY KEY AUTO_INCREMENT,"+
-    "name VARCHAR(255) NOT NULL,"+
-    "reps INT,"+
-    "weight INT,"+
-    "date DATE,"+
-    "lbs BOOLEAN)";
-    pool.query(createString, function(err){
-      context.results = "Table reset";
-      res.render('home',context);
-    })
+  console.log("insert person");
+  console.log("body: %j", req.body);
+    if(req.body.first_name != '' ){
+      pool.query("INSERT INTO `persons`(first_name,last_name,height,weight) VALUES (?,?, ?,?)",
+      [req.body.first_name, req.body.last_name, req.body.height, req.body.weight], function(err, result){
+        if(err){
+          next(err);
+          return;
+        }
+         res.render('home');
   });
+}else {
+  console.log("is empty");
+}
 });
 
-app.post('/insert',function(req,res,next){
+app.post('/insert_exercise',function(req,res,next){
   var context = {};
   console.log("body: %j", req.body)
-    if(req.body.name != '' ){
-      pool.query("INSERT INTO workouts (`name`,`reps`,`weight`, `date`, `lbs`) VALUES (?,?,?,?,?)",
-      [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.lbs], function(err, result){
+    if(req.body.workout != '' ){
+      pool.query("INSERT INTO `exercise`(workout,reps,muscle_group) VALUES (?,?,?);",
+      [req.body.workout, req.body.reps, req.body.muscle_group], function(err, result){
         if(err){
           next(err);
           return;
@@ -69,76 +70,149 @@ app.post('/insert',function(req,res,next){
 
 app.get('/',function(req,res,next){
   var context = {};
-  pool.query('SELECT * FROM workouts', function(err, rows, fields){
+  pool.query('SELECT  p.first_name, p.last_name, p.weight, e.workout, e.reps, wl.exercised_at, wl.workout_id FROM persons p \
+    INNER JOIN workout_log wl ON p.id = wl.person_id \
+    INNER JOIN exercise e ON wl.exercise_id = e.id', function(err, rows, fields){
     if(err){
       next(err);
       return;
     }
+
     context.results = rows;
     console.log(context.results);
     res.render('home', context);
   });
+
 });
 
-app.get('/updatedTable',function(req,res,next){
+
+app.get('/workoutlog',async function(req,res,next){
   var context = {};
-  pool.query('SELECT * FROM workouts', function(err, rows, fields){
+  pool.query('SELECT  p.first_name, p.last_name, p.weight, e.workout, e.reps, wl.exercised_at FROM persons p \
+    INNER JOIN workout_log wl ON p.id = wl.person_id \
+    INNER JOIN exercise e ON wl.exercise_id = e.id', (err, rows, fields) => {
+    if(err){
+      next(err);
+      return;
+    } 
+
+      pool.query('SELECT workout FROM exercise', (err, rows2, fields) => {
+      if(err){
+        next(err);
+        return;
+      }
+      
+        pool.query('SELECT first_name,last_name FROM persons',function(err,rows3,fields){
+          if(err){
+            next(err);
+            return
+          }
+        context.results = rows;
+        context.exercises = rows2;
+        context.people = rows3;
+        console.log(context.people);
+        res.render('workoutlog', context);
+        })
+   
+    });
+
+    
+  });
+
+context.results = rows;
+      context.exercises = rows2;
+      console.log(context.exercises);
+      res.render('workoutlog', context);
+  
+});
+  
+
+app.get('/insert_wo',function(req,res,next){
+  var context = {};
+  console.log("query: %j", req.query);
+  pool.query("INSERT INTO workout_log (person_id, exercise_id, exercised_at) Values \
+    ((select id from persons where first_name=? and last_name=?), \
+    (select id from exercise where workout=?), current_time);", [req.query.first_name,req.query.last_name,req.query.exercise], function(err, result){
+    
     if(err){
       next(err);
       return;
     }
-    context.results = rows;
-    console.log(context.results);
-    res.send(context);
+    res.render('workoutlog');
   });
 });
 
-
-app.get('/update',function(req,res,next){
+app.get('/foodlog',async function(req,res,next){
   var context = {};
-  pool.query("SELECT * FROM workouts WHERE id=?", [req.query.id], function(err, result){
+  pool.query('SELECT p.first_name, p.last_name, fs.food_desc, fs.calories, fc.eaten_at \
+              from persons p\
+              INNER JOIN food_consumption fc\
+              ON p.id = fc.p_id \
+              INNER JOIN food_selection fs \
+              ON fc.f_id = fs.id', (err, rows, fields) => {
+    if(err){
+      next(err);
+      return;
+    } 
+
+      pool.query('SELECT food_desc FROM food_selection', (err, rows2, fields) => {
+      if(err){
+        next(err);
+        return;
+      }
+      
+        pool.query('SELECT first_name,last_name FROM persons',function(err,rows3,fields){
+          if(err){
+            next(err);
+            return
+          }
+        context.results = rows;
+        context.selection = rows2;
+        context.people = rows3;
+        console.log(context.selection);
+        res.render('foodlog', context);
+        })
+   
+    });
+
+    
+  });
+
+      context.results = rows;
+      context.exercises = rows2;
+      console.log(context.exercises);
+      res.render('workoutlog', context);
+  
+});
+
+app.get('/insert_fl',function(req,res,next){
+  var context = {};
+  console.log("query: %j", req.query);
+  pool.query("INSERT INTO food_consumption (f_id,eaten_at, p_id) Values \
+    ((select id from food_selection where food_desc=?),current_time, \
+    (select id from persons where first_name=? and last_name=?));", [req.query.food_desc,req.query.first_name,req.query.last_name], function(err, result){
+    
     if(err){
       next(err);
       return;
     }
-    context.result = result; 
-    res.render('update',context);
+    res.render('foodlog');
   });
 });
 
-app.get('/safe-update',function(req,res,next){
-  var context = {};
-  pool.query("SELECT * FROM workouts WHERE id=?", [req.query.id], function(err, result){
-    console.log("in the safe update");
-    if(err){
-      next(err);
-      return;
-    }
-    if(result.length == 1){
-      pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
-        [req.query.name, req.query.reps, req.query.weight ,req.query.date,req.query.lbs ,req.query.id],
-        function(err, result){
-        if(err){
-          next(err);
-          return;
-        }
-        res.render('home');
-      });
-    }
-  });
-});
-
-app.get('/delete',function(req,res,nest){
-  pool.query("delete from workouts where id=?", [req.query.id], function(err,result){
+app.get('/delete_wo',function(req,res,next){
+  console.log(req.query.id)
+  pool.query("delete from workout_log where workout_id=?", [req.query.id], function(err,result){
     console.log("in the delete function");
   if(err){
     next(err);
     return;
   }
+  res.render('home');
   });
 });
 
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http://flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
